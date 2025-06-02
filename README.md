ed82ee6d2c599149f467ef14b10d78267c771429
Shannel Feranand <shannelferanand@Shannels-MacBook-Pro.local>
2025-06-02 07:59:46 +0100

    38 test passed, 2 tests left for data insertion

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+- Instructions for this sprint
+1. clone repo
+2. Run `npm install
+3. Run `npm install dotenv
+
+4. Run setup script to create database
+     - npm run setup-dbs
+
+5. create two .env files to connect to both databases
+     - .env.test
+     - .env.development
+
+6. Verify setup
+     - npm run test_seed
+     - npm run seed_dev
+
+        
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,57 @@
 const db = require("../connection")
+const format = require ('pg-format');
+const articles = require("../data/test-data/articles");
+const { convertTimestampToDate } = require("./utils");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db.query(`DROP TABLE IF EXISTS comments`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles`)
+    }).then(() => {
+      return db.query(`DROP TABLE IF EXISTS users`)
+    }).then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics`)
+    }).then(() => {
+      return db.query(`CREATE TABLE topics(slug VARCHAR(50) PRIMARY KEY,description VARCHAR, img_url VARCHAR(1000))`)
+    }).then(() => {
+      return db.query(`CREATE TABLE users(username VARCHAR(50) PRIMARY KEY,name VARCHAR(50), avatar_url VARCHAR(1000))`)
+    }).then(() => {
+      return db.query(`CREATE TABLE articles(
+                       article_id SERIAL PRIMARY KEY, title VARCHAR(200), topic VARCHAR(50) REFERENCES topics(slug), 
+                       author VARCHAR(50) REFERENCES users(username), body TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                       votes INT DEFAULT 0, article_img_url VARCHAR(1000))`
+                      )
+    }).then(() => {
+      return db.query(`CREATE TABLE comments(
+                       comment_id SERIAL PRIMARY KEY, article_id INT REFERENCES articles(article_id), 
+                       body TEXT, votes INT DEFAULT 0, author VARCHAR(50) REFERENCES users(username), 
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+                      )
+    }).then(() => {
+      const formattedTopicValues = topicData.map(({description, slug, img_url}) => {
+        return [description, slug, img_url]
+      })
+      const sqlTopicString = format(`INSERT INTO topics(description, slug, img_url) VALUES %L`, formattedTopicValues)
+       //console.log(sqlTopicString)
+       return db.query(sqlTopicString)   
+    }).then(() => {
+      const formattedUserValues = userData.map(({username, name, avatar_url}) => {
+        return [username, name, avatar_url]
+      })
+      const sqlUserString = format(`INSERT INTO users(username, name, avatar_url) VALUES %L`, formattedUserValues)
+       //console.log(sqlUserString)
+       return db.query(sqlUserString)   
+    }).then(() => {
+      const formattedArticleValues = articleData.map(({article_id, title, topic, author, body, created_at, votes, article_img_url}) => {
+        return [article_id, title, topic, author, body, created_at, votes, article_img_url]
+      })
+      const sqlArticleString = format(`INSERT INTO articles(article_id, title, topic, author, body, created_at, votes, article_img_url) VALUES %L`, formattedArticleValues)
+       console.log(sqlArticleString)
+       //return db.query(sqlArticleString)   
+    })
+    
+    
 };
+
+
 module.exports = seed;
