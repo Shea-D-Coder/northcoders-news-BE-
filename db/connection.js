6e711f2a43801d337426ee9c3531413d97def365
Shannel Feranand <shannelferanand@Shannels-MacBook-Pro.local>
2025-06-10 20:39:57 +0100

    Set up a database instance using Supabase

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,14 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
+
+require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL ) {
+    throw new Error("PGDATABASE or DATABASE_URL not set")
+} 
+
 const config = {};
 
 if (ENV === "production") {
@@ -9,16 +17,4 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-
-
-const db = new Pool();
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL ) {
-    throw new Error("PGDATABASE or DATABASE_URL not set")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
-
-module.exports = new Pool(config);
+module.exports =  new Pool(config);
